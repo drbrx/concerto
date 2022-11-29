73cf2bc497025e85074bfc922e0824bd94bf32d1
Joel bosio <bosio.joel@volta.ts.it>
2022-11-29 09:46:21 +0100

    structured index and prepared order page

--- dbConnector.php
+++ dbConnector.php
@@ -7,4 +7,4 @@ $_SESSION['pwd'] = "";
 $_SESSION['db'] = "concertobosio";
 
 
-$db = mysqli_connect($_SESSION['address'], $_SESSION['user'], $_SESSION['pwd'], $_SESSION['db']) or die("Error 418: I'm a teapot! I don't make coffee and consequently didn't connect to the db!");
+$dbConcerto = mysqli_connect($_SESSION['address'], $_SESSION['user'], $_SESSION['pwd'], $_SESSION['db']) or die("Error 418: I'm a teapot! I don't make coffee and consequently didn't connect to the db!");
--- index.php
+++ index.php
@@ -1,4 +1,38 @@
-<?php
-require_once("dbConnector.php")
+<head>
+    <title>Concerti</title>
+    <meta charset="utf-8">
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel="stylesheet" href="style.css">
+    <title></title>
+</head>
 
-?>
+<body>
+    <?php
+    require_once("dbConnector.php");
+
+    $query = "SELECT * FROM teventi";
+    $rec = mysqli_query($dbConcerto, $query) or die($query);
+    if (mysqli_num_rows($rec) > 0) {
+        $placeOnLeft = false;
+        while ($currentRecord = mysqli_fetch_array($rec)) {
+            $placeOnLeft = !$placeOnLeft;
+            $idEvento = $currentRecord["id"];
+            echo "
+            <div class=\"evento\" style=\"float:" . ($placeOnLeft ? "left\"" : "right\"") . ">"
+                . "<div class=\"eventImage\">"
+                . "<img src=\"" . $currentRecord['imgPath'] . "\"></img>"
+                . "</div>"
+                . "Data: " . $currentRecord['data'] . "<br></br>"
+                . "Posti Disponibili: " . $currentRecord['posti'] . "<br></br>"
+                . "Prenota: " . "<input type=\"button\" onclick=\"location.href='prenota.php?evento=" . $currentRecord['id'] . "'\" value=\"" . $currentRecord['costoBiglietto'] . "\"/>"
+                . "<br></br>"
+                . "</div>";
+            if (!$placeOnLeft) {
+                echo "<div style=\"clear: both;\"></div>";
+            }
+        }
+    } else {
+        echo "Non sono stati trovati eventi. Prova a ricontrollare più tardi";
+    }
+    ?>
+</body>
